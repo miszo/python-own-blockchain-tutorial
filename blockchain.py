549b48cf8b9ddebd2320d897c5693d1f422037b4
Miszo Radomski <miszo.radomski@gmail.com>
2018-09-13 23:31:44 +0200

    Adding some documentation to functions

--- blockchain.py
+++ blockchain.py
@@ -1,20 +1,30 @@
+# Initializing blockchain list
 blockchain = []
 
 
 def get_last_blockchain_value():
+    """ Returns last value of the current blockchain """
     return blockchain[-1]
 
 
 def add_value(transaction_amount, last_transaction=[1]):
+    """ Append a new transaction to the blockchain with last blockchain value
+
+    Arguments:
+        :transaction_amount: The amount that should be added.
+        :last_transaction: The last blockchain transaction (default[1]).
+    """
     blockchain.append([last_transaction, transaction_amount])
 
 
 def get_user_input():
+    """ Asks user for transaction amount and returns it as a float """
     return float(input('Your transaction amount please: '))
 
 
 add_value(get_user_input())
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=get_user_input())
+add_value(last_transaction=get_last_blockchain_value(),
+          transaction_amount=get_user_input())
 add_value(get_user_input(), get_last_blockchain_value())
 
 print(blockchain)
